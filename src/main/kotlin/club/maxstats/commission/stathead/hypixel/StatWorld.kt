8c9cde2437067e5ecfba332c957267e8edaeec82
exejar <56853437+exejar@users.noreply.github.com>
2023-09-30 20:13:16 -0400

    Added Hypixel information variables

--- src/main/kotlin/club/maxstats/commission/stathead/hypixel/StatWorld.kt
+++ src/main/kotlin/club/maxstats/commission/stathead/hypixel/StatWorld.kt
@@ -4,10 +4,13 @@ import net.minecraft.entity.player.EntityPlayer
 
 object StatWorld {
     val statPlayers: HashMap<EntityPlayer, StatPlayer> = HashMap()
+    var locrawInfo: LocrawInfo? = null
+    var isHypixel: Boolean = false
+    val gameMode: GameType get() = locrawInfo?.gametype ?: GameType.UNKNOWN
+    val inGame: Boolean get() = locrawInfo?.inGame() ?: false
 }
-
 data class StatPlayer(
     val uuid: String,
     val name: String,
-    val stats: StatsGeneral
+    val stats: HypixelPlayer
 )
